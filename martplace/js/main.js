7845f202983348f4fc10077d8dc4d0ab940f7653
valentine-samoylov <valentine-samoylov@gmail.com>
2020-10-08 20:56:21 +0300

    Finished Added Footer, Sell-buy, Join sections

--- martplace/js/main.js
+++ martplace/js/main.js
@@ -6,52 +6,15 @@ $(".product-card__rating").rateYo({
 	starWidth: "15px",
 	spacing: "3px",
 	rating: 4.5,
+	readOnly: true,
 });
 
 $(".section-filter__btn").click(function () {
-	$(".section-filter__dropdown").slideToggle("slow", function () {
-	});
+	$(".section-filter__dropdown").slideToggle("slow", function () {});
 });
 
 // Mixitup
-var mixer = mixitup('.newest__items');
-
-//<Counter>====================================================================================
-// $(window).scroll(testScroll);
-// var viewed = false;
-
-// function isScrolledIntoView(elem) {
-// 	var docViewTop = $(window).scrollTop();
-// 	var docViewBottom = docViewTop + $(window).height();
-
-// 	var elemTop = $(elem).offset().top;
-// 	var elemBottom = elemTop + $(elem).height();
-
-// 	return elemBottom <= docViewBottom && elemTop >= docViewTop;
-// }
-
-// function testScroll() {
-// 	if (isScrolledIntoView($(".stats")) && !viewed) {
-// 		viewed = true;
-// 		$(".stats__number").each(function () {
-// 			$(this)
-// 				.prop("Counter", 0)
-// 				.animate(
-// 					{
-// 						Counter: $(this).text(),
-// 					},
-// 					{
-// 						duration: 4000,
-// 						easing: "swing",
-// 						step: function (now) {
-// 							$(this).text(Math.ceil(now));
-// 						},
-// 					}
-// 				);
-// 		});
-// 	}
-// }
-//</Counter>===================================================================================
+var mixer = mixitup(".js-newest-mixitup");
 
 // Weekly Product
 var weeklyProduct = new Swiper(".js-weekly-slider", {
